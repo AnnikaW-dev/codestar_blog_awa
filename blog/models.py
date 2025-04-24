4c3338eb41454030b462b88c1926dde7e46091db
AnnikaW-dev <xwtuwqcj@students.codeinstitute.net>
2025-04-24 15:58:18 +0200

    Template inheritance

--- blog/models.py
+++ blog/models.py
@@ -12,13 +12,13 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    update_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
     
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"{self.title} | writen by {self.title}"
+        return f"{self.title} | writen by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
